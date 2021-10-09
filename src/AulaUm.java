c4caf765be4282830e2e8e784072d2ec2ee32574
Cassiano Augusto da Silva <cassianoas@hotmail.com>
2021-10-09 14:01:26 -0300

    Java Basico

---
+++ src/AulaUm.java
@@ -0,0 +1,10 @@
+
+public class AulaUm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Wold");
+		
+	}
+
+}
---
+++ src/TipoVariaveis.java
@@ -0,0 +1,24 @@
+
+public class TipoVariaveis {
+
+	public static void main(String[] args) {
+		byte tipoByte = 123;
+		short tipoShort = 5555;
+		char tipoChar = 'G';
+		float tipoFloat = 2.1f;
+		double tipoDouble = 1.52;
+		int tipoInt = 5131236;
+		long tipoLong = 1234567890l;
+		boolean tipoBooleano = true;
+		System.out.println("Valor do tipoByte = " + tipoByte);
+		System.out.println("Valor do tipoShort = " + tipoShort);
+		System.out.println("Valor do tipoChar = "  + tipoChar);
+		System.out.println("Valor do tipoFloat = " +tipoFloat);
+		System.out.println("Valor do tipoDouble = " +tipoDouble);
+		System.out.println("Valor do tipoInt = " +tipoInt);
+		System.out.println("Valor do tipoLong = " +tipoLong);
+		System.out.println("Valor do tipoBooleano = " +tipoBooleano);
+
+	}
+
+}
